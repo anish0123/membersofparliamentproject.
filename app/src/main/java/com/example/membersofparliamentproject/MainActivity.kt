e0cba6ceef689e834fb036fde960d9b5d8d6d835
anish0123 <anishmhn@hotmail.com>
2022-09-28 14:11:30 +0300

    created fragments and added parliament file for testing

--- app/src/main/java/com/example/membersofparliamentproject/MainActivity.kt
+++ app/src/main/java/com/example/membersofparliamentproject/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.membersofparliamentproject
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.MediaController
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.membersofparliamentproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
     }
 }
